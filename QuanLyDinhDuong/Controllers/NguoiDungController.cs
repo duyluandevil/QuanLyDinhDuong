35cfb0d93e018d31edb6697a67070a74ca59fc52
Admin <Admin@LUAN>
2021-07-04 23:48:57 +0700

    4/7/2021 Update #3

---
+++ QuanLyDinhDuong/Controllers/NguoiDungController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyDinhDuong.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        // GET: NguoiDung
+        public ActionResult Dangnhap()
+        {
+            return View();
+        }
+
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+    }
+}
