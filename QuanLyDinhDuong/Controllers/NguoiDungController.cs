b65c0a3e28faed02ddf3548dcbff7f72d08b84bc
Kenru Vương Hoàng <vuonghoangnhuquynh@gmail.com>
2021-07-07 23:19:13 +0700

    add đăng ký tài khoản

--- QuanLyDinhDuong/Controllers/NguoiDungController.cs
+++ QuanLyDinhDuong/Controllers/NguoiDungController.cs
@@ -20,10 +20,76 @@ namespace QuanLyDinhDuong.Controllers
         {
             return View();
         }
-        //public ActionResult Dangnhap(FormCollection collection,TAIKHOAN tk)
-        //{
+        [HttpPost]
+        public ActionResult Dangnhap(FormCollection collection, TAIKHOAN tk)
+        {
+            var taikhoan = collection["TaiKhoan"];
+            var matkhau = collection["MatKhau"];
+            var nhaplaimk = collection["NhapLaiMatKhau"];
+            var email = collection["email"];
+            var sdt = collection["sdt"];
+            var ngaysinh = String.Format("{0:MM/dd/yyyy}", collection["ngaysinh"]);
+            var gioitinh = collection["RadioGioiTinh"];
+            if (String.IsNullOrEmpty(taikhoan))
+            {
+                ViewData["Loi3"] = "Bạn chưa nhập tên tài khoản";
+            }
+            else
+            {
+                TAIKHOAN taik = data.TAIKHOANs.SingleOrDefault(t => t.IDTAIKHOAN == taikhoan);
+                if (taik != null)
+                {
+                    ViewBag.ThongBao = "Tên tài khoản đã tồn tại";
+                }
+            }
+            if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi4"] = "Bạn chưa nhập mật khẩu";
+            }
+            if (String.IsNullOrEmpty(nhaplaimk))
+            {
+                ViewData["Loi5"] = "Bạn chưa nhập lại mật khẩu";
+            }
+            else if (matkhau != nhaplaimk)
+            {
+                ViewBag.ThongBao = "Mật khẩu nhập lại không đúng";
+            }
+            if (String.IsNullOrEmpty(email))
+            {
+                ViewData["Loi6"] = "Bạn chưa nhập Email";
+            }
+            if (String.IsNullOrEmpty(sdt))
+            {
+                ViewData["Loi7"] = "Bạn chưa nhập số điện thoại";
+            }
+            if (String.IsNullOrEmpty(ngaysinh))
+            {
+                ViewData["Loi8"] = "Bạn chưa chọn ngày sinh";
+            }
+            else
+            {
+                tk.IDTAIKHOAN = taikhoan;
+                tk.MATKHAU = matkhau;
+                tk.EMAIL = email;
+                tk.SDT = sdt;
+                tk.GIOITINH = gioitinh;
+                tk.NGAYSINH = DateTime.Parse(ngaysinh);
+                data.TAIKHOANs.InsertOnSubmit(tk);
+                data.SubmitChanges();
+                return View("Index");
+            }
+            //else if (ngaysinh)
+            //{
 
-        //    return this.Dangnhap();
-        //}
+            //}
+            return this.Dangnhap();
+        }
+
+
+        public ActionResult TrangCaNhan()
+        {
+            return View();
+        }
+        // 82a71b7698b5168ad94da2e6bb4f364248f19cc3
     }
 }
